db9c2185c78ce6c58359fcd4d622e7d4e3055f0d
Rebecca Folmer Schade <rebexxi@yahoo.dk>
2025-12-03 15:37:43 +0100

    working with the memory movement system

---
+++ Assets/_Project/Scripts/Debugs/debug_load_scene.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class debug_load_scene : MonoBehaviour
+{
+    public void loadjanitor_memory()
+    {
+        SceneManager.LoadScene("Janitor_Memory_World");
+    }
+
+}
