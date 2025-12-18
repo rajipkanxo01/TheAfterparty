93ac87ec2c0963211f3dcbe8965cf874feae4351
Rajib Paudyal <pdlrajip12@gmail.com>
2025-12-18 13:26:22 +0545

    Added scene transitions for Chapter 4 and implemented quit game functionality

---
+++ Assets/_Project/Scripts/Presentation/QuitGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Presentation
+{
+    public class QuitGame : MonoBehaviour
+    {
+        public void Quit()
+        {
+            Debug.Log("Quitting game...");
+            UnityEngine.Application.Quit(0);
+        }
+    }
+}
